54f7fe64ce85c42954a3aaa1cfe0e59cec94f039
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-08-29 12:34:37 +0200

    initial commit

---
+++ js/spritemap.js
@@ -0,0 +1,13 @@
+(self["webpackChunkmix_loc"] = self["webpackChunkmix_loc"] || []).push([["spritemap"],{
+
+/***/ "?c20d":
+/*!******************************!*\
+  !*** spritemap-dummy-module ***!
+  \******************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+}]);
